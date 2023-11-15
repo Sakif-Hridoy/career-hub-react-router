377a196609f7a0d6bb59cc277c7c636b93ace6ba
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-11-15 15:54:14 +0600

    add routes and config dynamic navbar,add components

---
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AppliedJobs = props => {
+    return (
+        <div>
+           <h2>Jobs i applied</h2> 
+        </div>
+    );
+};
+
+AppliedJobs.propTypes = {
+    
+};
+
+export default AppliedJobs;
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ErrorPage = props => {
+    return (
+        <div>
+            <h2>Sorry</h2>
+        </div>
+    );
+};
+
+ErrorPage.propTypes = {
+    
+};
+
+export default ErrorPage;
