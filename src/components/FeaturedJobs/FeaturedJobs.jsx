08e5beb15bb1f77c6e9500c0199504003aff7cba
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-11-16 10:00:38 +0600

    show sobs page data

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,8 @@ import Job from '../Job/Job'
 const FeaturedJobs = () => {
     const [jobs,setJobs] = useState([]);
 
+    const [dataLength,setDatalength] = useState(4);
+
     useEffect(()=>{
         fetch('jobs.json')
         .then(res=>res.json())
@@ -12,14 +14,20 @@ const FeaturedJobs = () => {
     },[])
 
     return (
-        <div>
-            <h2 className='text-5xl text-center'>Featured Jobs:{jobs.length}</h2>
+        <div className=''>
+           <div className='text-center'>
+           <h2 className='text-5xl '>Featured Jobs:{jobs.length}</h2>
             <p className='text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+           </div>
+
+            <div className='grid grid-cols-2 gap-6'>
+                {
+                    jobs.slice(0,dataLength).map(job=><Job key={job.id} job={job}></Job>)
+                }
+            </div>
 
             <div>
-                {
-                    jobs.map(job=><Job key={job.id} job={job}></Job>)
-                }
+                <button className='btn'>Show All Jobs</button>
             </div>
 
         </div>
